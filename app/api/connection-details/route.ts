4cecd605870746d59c2dcb81944a6773a67bc618
Rohit menon <140264361+rohitmenonhart-xhunter@users.noreply.github.com>
2024-11-17 11:55:14 +0530

    Update route.ts

--- app/api/connection-details/route.ts
+++ app/api/connection-details/route.ts
@@ -59,7 +59,7 @@ function createParticipantToken(
 ) {
   const at = new AccessToken(API_KEY, API_SECRET, {
     ...userInfo,
-    ttl: "15m",
+    ttl: "3d",
   });
   const grant: VideoGrant = {
     room: roomName,
