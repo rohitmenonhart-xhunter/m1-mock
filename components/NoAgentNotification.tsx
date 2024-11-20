3303ebbdab1244f8f120dc87380e8f0dc690c25c
Rohit menon <140264361+rohitmenonhart-xhunter@users.noreply.github.com>
2024-11-20 18:11:39 +0530

    Update NoAgentNotification.tsx

--- components/NoAgentNotification.tsx
+++ components/NoAgentNotification.tsx
@@ -70,13 +70,7 @@ export function NoAgentNotification(props: NoAgentNotificationProps) {
             It&apos;s quiet... too quiet. Is your agent lost? Ensure your agent
             is properly configured and running on your machine.
           </p>
-          <a
-            href="https://docs.livekit.io/agents/quickstarts/s2s/"
-            target="_blank"
-            className="underline whitespace-nowrap"
-          >
-            View guide
-          </a>
+          
           <button onClick={() => setShowNotification(false)}>
             {/* Close Icon */}
             <svg
